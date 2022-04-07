b660232a59b4ee8472805cd8026959e379821322
autotntfan <jam310157@gmail.com>
2022-04-07 14:18:14 +0800

    Update HW1_Part1_Template_Spring2022.m

--- HW1-1/HW1_Part1_Template_Spring2022.m
+++ HW1-1/HW1_Part1_Template_Spring2022.m
@@ -6,29 +6,31 @@
 %										Modified by Meng-Lin Li, 03/17/2022
 %												    Dept. of Electrical Engineering
 %													National Tsing Hua University, Taiwan
-
+clear
+close all
 
 % Note that you can use "SI" unit instead of the units I used below
 time_offset = 6.48; % in usec
 fs = 50;    % sampling rate, in MHz
-aper_size = ??;  % aperture size, in mm
-focal_pt = ??;   % focal point, in mm
+% aper_size = ??;  % aperture size, in mm
+focal_pt = 12;   % focal point, in mm
 dx = 0.050;    % distance between two successive scanning positions, in mm
-soundv = ??;  % speed of sound, in mm/us
+soundv = 1.5;  % speed of sound, in mm/us
+pt_location = [6 9 12 15 18]; % points location in mm
 
 % (a)
-DR = ?;    % dynamic range, in dB
+DR = 45;    % dynamic range, in dB
 load('points_rf_data');
 rf_data = points_rf_data; 
 clear points_rf_data;
 [m,n] = size(rf_data);
 time_axis = time_offset + (0:1:(m-1))*(1/fs); % in usec
-z_axis = ?;   % z axis
-x_axis = ?;    % x axis
-envelope = abs(?);   % envelope detection
-envelope_dB = ?;    % log conversion with respect to the maximum value 
+z_axis = soundv*time_axis./2;   % z axis
+x_axis = (0:1:(n-1))*dx;    % x axis
+envelope = abs(hilbert(rf_data));   % envelope detection
+envelope_dB = 20*log(envelope/max(max(envelope))+eps);    % log conversion with respect to the maximum value 
 figure
-image(?); % or imagesc()?
+image(x_axis, z_axis, envelope_dB+DR); % or imagesc()?
 colormap(gray(DR))
 colorbar;
 title('Point targets')
@@ -42,7 +44,7 @@ axis image
 
 % (c)
 % Based on your answer in (b), tell the speed of sound
-soundv = ??;  % speed of sound, in mm/us
+% soundv = ??;  % speed of sound, in mm/us
 
 % (d)
 % Check the spatial resolution based on the fundamental definition of the spatial resolution and your eye examination
@@ -68,45 +70,59 @@ soundv = ??;  % speed of sound, in mm/us
 % idx = find(PSF >= 0.5); % PSF is the projected PSF after interpolation
 % FWHM_PSF = (idx(end) - idx(1))*spatial sampling interval;  % in um
 
-
-% (f)
-% Find the aperture size of the transducer based on the theoretic lateral resolution "at the focal point"
-fc = ?; % center freq. of the transducer, in MHz, try "fft" the provided RF data to find out the center frequency. Give it a thought: Which A-line and which point-target data should you use? Again, remember to elaborate this point in your report.
-lambda = ?; % wavelength of the center frequency
-depth = ?;
-f_number = ?;
-aper_size = ??;  % aperture size, in mm
-
-
-% (g)
-
-% (h)
-
-% (i)
-N = ??; % number of scatterers
-scatterer_pos_x = rand(N, 1)*scale; % scale: used to scale the value to within 4 cm, x position, position unit or lateral beam spacing (i.e., lateral spatial sampling interval) has to be the same as that of PSF
-scatterer_pos_z = rand(N, 1)*scale; % scale: used to scale the value to within 4 cm, z position, position unit or axial sampling interval has to be the same as that of PSF
-% or
-% scatter_pos = rand(N)*scale;
-scatterer_dist = zeros(Nz,Nx); % spatial distribution of the scatterers, 
-                               % Nz: grid points along the z direction, determined by the "4 cm" field of view in z and the spatial sampling interval in the z direction
-							   % Nx: grid points along the x direction, determined by the "4 cm" field of view in x and the spatial sampling interval in the x direction
-
-% locate the scatterers into the scatterer distribution matrix
-for iX = 1:Nx,
-	for iZ = 1:Nz,
-		if ??? % if the grid matches the scatterer position (scatter_pos_x, scatter_pos_z)
-			scatterer_dist(iZ, iX) = 1; % assign the same back-scattering coef.
-		end
-	end
+lateral_project = max(envelope,[],2);
+figure
+for ii = 1:length(pt_location)
+    PSF = lateral_project(floor(1+(ii-1)*m/length(pt_location)):floor(ii*m/length(pt_location)));
+    lateral_res = sum(PSF > 0.5*max(PSF)) * dz;
+    subplot(1,5,ii)
+    plot(project)
 end
+figure
+plot(z_axis,lateral_project)
 
-RF_image = conv2(scatterer_dist, RF_PSF); % note that the sampling interval (in x and in z) of the scatterer distribution should be the same as that of RF_PSF.
-                                          % RF_PSF: PSF in RF data form
-										  % RF_image: RF data
-% then envelope detection with Hilbert transform, log conversion, and then determine the image dynamic range to show the image 
-% then estimate the contrast
-
-
-% (j) Bonus
-
+axial_project = max(envelope,[],1);
+figure
+plot(x_axis, axial_project)
+% 
+% % (f)
+% % Find the aperture size of the transducer based on the theoretic lateral resolution "at the focal point"
+% fc = ?; % center freq. of the transducer, in MHz, try "fft" the provided RF data to find out the center frequency. Give it a thought: Which A-line and which point-target data should you use? Again, remember to elaborate this point in your report.
+% lambda = ?; % wavelength of the center frequency
+% depth = ?;
+% f_number = ?;
+% aper_size = ??;  % aperture size, in mm
+% 
+% 
+% % (g)
+% 
+% % (h)
+% 
+% % (i)
+% N = ??; % number of scatterers
+% scatterer_pos_x = rand(N, 1)*scale; % scale: used to scale the value to within 4 cm, x position, position unit or lateral beam spacing (i.e., lateral spatial sampling interval) has to be the same as that of PSF
+% scatterer_pos_z = rand(N, 1)*scale; % scale: used to scale the value to within 4 cm, z position, position unit or axial sampling interval has to be the same as that of PSF
+% % or
+% % scatter_pos = rand(N)*scale;
+% scatterer_dist = zeros(Nz,Nx); % spatial distribution of the scatterers, 
+%                                % Nz: grid points along the z direction, determined by the "4 cm" field of view in z and the spatial sampling interval in the z direction
+% 							   % Nx: grid points along the x direction, determined by the "4 cm" field of view in x and the spatial sampling interval in the x direction
+% 
+% % locate the scatterers into the scatterer distribution matrix
+% for iX = 1:Nx,
+% 	for iZ = 1:Nz,
+% 		if ??? % if the grid matches the scatterer position (scatter_pos_x, scatter_pos_z)
+% 			scatterer_dist(iZ, iX) = 1; % assign the same back-scattering coef.
+% 		end
+% 	end
+% end
+% 
+% RF_image = conv2(scatterer_dist, RF_PSF); % note that the sampling interval (in x and in z) of the scatterer distribution should be the same as that of RF_PSF.
+%                                           % RF_PSF: PSF in RF data form
+% 										  % RF_image: RF data
+% % then envelope detection with Hilbert transform, log conversion, and then determine the image dynamic range to show the image 
+% % then estimate the contrast
+% 
+% 
+% % (j) Bonus
+% 
